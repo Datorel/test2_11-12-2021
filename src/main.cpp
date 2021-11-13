b0b65b7a77ece5a81fd62d37d5acf3be8d1c1c5d
morpheeus543 <A543432321234@gmail.com>
2021-11-13 11:31:08 -0500

    Added comment to driver code on line 93

--- src/main.cpp
+++ src/main.cpp
@@ -23,7 +23,6 @@ void on_center_button() {
  * to keep execution time for this mode under a few seconds.
  */
 void initialize() {
-
 	pros::lcd::initialize();
 	pros::lcd::set_text(1, "Hello PROS User!");
 
@@ -79,7 +78,6 @@ void opcontrol() {
 	pros::Motor left_mtr(1);
 	pros::Motor right_mtr(2);
 
-	//this is a test
 	while (true) {
 		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
 		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
@@ -92,3 +90,4 @@ void opcontrol() {
 		pros::delay(20);
 	}
 }
+// testing on line 93
