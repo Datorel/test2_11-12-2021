c4b4888fc21922c7f4d61be8585dede8aec6fa52
toddo1225 <toddo1225@gmail.com>
2021-11-13 12:38:55 -0500

    add working pot code to print on line 4

--- src/main.cpp
+++ src/main.cpp
@@ -77,23 +77,23 @@ void opcontrol() {
 	pros::Controller master(pros::E_CONTROLLER_MASTER);
 	pros::Motor left_mtr(1);
 	pros::Motor right_mtr(2);
-	pros::ADIDigitalIn pot ('H');
+	pros::ADIAnalogIn pot ('H');
 
 	while (true) {
 		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
 		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
 		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);
 
-			pros::lcd::clear_line(1);
-			pros::lcd::print(1, "%d", pot.get_value());
+			pros::lcd::clear_line(4);
+			pros::lcd::print(4, "%d", pot.get_value());
 
-	
+
 
 		int left = master.get_analog(ANALOG_LEFT_Y);
 		int right = master.get_analog(ANALOG_RIGHT_Y);
 
 		left_mtr = left;
 		right_mtr = right;
-		pros::delay(20);
+		pros::delay(100);
 	}
 }
